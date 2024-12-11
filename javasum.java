4711895724e682a0b177c8e807d78b02272665e5
PraggyaAmitabh <praggyaamitabh16@gmail.com>
2024-12-12 00:48:05 +0530

    added a message to javasum.java

--- javasum.java
+++ javasum.java
@@ -9,7 +9,8 @@ System.out.println("enter no");
 Scanner sc = new Scanner(System.in);
 a=sc.nextInt();
 b=sc.nextInt();
-System.out.println(a+b);}
+System.out.println("the sum is " +(a+b));
+}
 }
 
 
