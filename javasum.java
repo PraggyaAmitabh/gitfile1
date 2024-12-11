9a21ccb5091622c8474410f09494cb18c88cb2c3
PraggyaAmitabh <praggyaamitabh16@gmail.com>
2024-12-12 00:41:18 +0530

    initial commit

---
+++ javasum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class javasum 
+{
+
+    public static void main(String args[])
+    {//System.out.println("Learn Coding");
+int a,b;
+System.out.println("enter no");
+Scanner sc = new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+System.out.println(a+b);}
+}
+
+
+    
